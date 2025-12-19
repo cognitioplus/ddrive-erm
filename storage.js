2a616734575f4588f4e835b8257a8bb3556f76ca
Cognitio+ <132303904+cognitioplus@users.noreply.github.com>
2025-12-19 11:50:36 +0800

    Create storage.js

---
+++ storage.js
@@ -0,0 +1,15 @@
+{
+  "name": "ddrive-m-pwa",
+  "version": "1.0.0",
+  "description": "Progressive Web App for DDRiVE-M Platform",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "engines": {
+    "node": ">=14.0.0"
+  }
+}
